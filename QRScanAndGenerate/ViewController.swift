421e844e9b66833e54fb3163693482c9694de82c
Greg Hughes <greghughes988@gmail.com>
2019-04-24 10:39:44 -0600

    Initial Commit

---
+++ QRScanAndGenerate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScanAndGenerate
+//
+//  Created by Greg Hughes on 4/24/19.
+//  Copyright © 2019 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
